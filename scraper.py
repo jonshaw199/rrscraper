157e4f44336baa350c282823505c909b84a168ef
Jon <jonshaw199@gmail.com>
2025-03-19 16:41:41 -0700

    improve default directory name

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import bs4
+from datetime import datetime
 import os
 import requests
 import time
@@ -8,7 +9,7 @@ class Scraper:
     def __init__(self, url: str, out_dir: str = None):
         html = requests.get(url).text
         self.soup = bs4.BeautifulSoup(html, "html.parser")
-        self.out_dir = f"data/{out_dir or str(time.time())}"
+        self.out_dir = f"data/{out_dir or datetime.now().strftime('%Y%m%d_%H%M%S')}"
 
     def scrape(self):
         os.makedirs(self.out_dir, exist_ok=True)
