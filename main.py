79add6dbfb7a7b7a62c29aab44adbdfc380274ac
Jon <jonshaw199@gmail.com>
2025-03-18 21:52:29 -0700

    formatter

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from argparse import ArgumentParser
-from SystemScraper import SystemScraper
+
+from op25formatter import Op25Formatter
+from systemscraper import SystemScraper
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--url", help="Radio Reference URL", required=True)
     parser.add_argument("--outdir", help="Name of out directory")
+    parser.add_argument("--format", help="Data format")
     args = parser.parse_args()
 
     if "/sid" in args.url:
-        SystemScraper(args.url).scrape()
+        scraper = SystemScraper(args.url, args.outdir)
+        scraper.scrape()
+        if args.format == "op25":
+            Op25Formatter(scraper.outdir).format()
     else:
         raise ValueError("Can't scrape this URL")
